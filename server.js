5763d3643cea16c0736a9be249fa04d192b55592
Carolina Madella <carolina.madella@gmail.com>
2024-06-01 19:01:08 -0500

    added artwork routes

--- server.js
+++ server.js
@@ -32,6 +32,12 @@ const options = {
 
 const swaggerSpec = swaggerJSDoc(options);
 
+/*******************
+ *
+ * ARTIST
+ *
+ *******************/
+
 //Get all artists
 app.get("/artists", async (req, res) => {
   // copied from ChatGPT the code below
@@ -136,6 +142,120 @@ app.delete("/artists/:id", async (req, res, next) => {
     res.status(200).json({ message: "artist deleted successfully" });
   } catch (error) {
     console.error("Error deleting artist:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+/*******************
+ *
+ * ARTWORKS
+ *
+ *******************/
+
+//Get all artworks
+app.get("/artworks", async (req, res) => {
+  // copied from ChatGPT the code below
+  try {
+    const db = await mongodb.connectDB();
+
+    // Get a reference to the collection
+    const collection = db.collection("artworks");
+
+    // Find documents in the collection
+    const artworks = await collection.find().toArray();
+
+    console.log(artworks);
+
+    res.status(201).json(artworks);
+  } catch (err) {
+    console.error("Error reading documents:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/artworks", async (req, res) => {
+  // copied from ChatGPT some of the code below
+  // Assuming the request body contains the artwork information
+  const newArtwork = req.body;
+  console.log("Data body", newArtwork);
+
+  try {
+    const db = await mongodb.connectDB();
+    const collection = db.collection("artworks");
+
+    // Insert the new artwork into the collection
+    const result = await collection.insertOne(newArtwork);
+    const artworkID = result.insertedId.toString();
+
+    res
+      .status(201)
+      .json({ message: "Artwork inserted successfully", id: artworkID });
+  } catch (error) {
+    console.error("Error inserting artwork:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.put("/artworks/:id", async (req, res) => {
+  const artworkId = req.params.id;
+  const updatedArtwork = req.body;
+
+  //data validation
+  if (artworkId == null || updatedArtwork == null) {
+    return res.status(404).json({ error: "invalid input provided" });
+  }
+
+  try {
+    const db = await mongodb.connectDB();
+    const collection = db.collection("artworks");
+
+    // Update the artwork by ID
+    const result = await collection.findOneAndUpdate(
+      { _id: new mongodb.ObjectId(artworkId) },
+      { $set: updatedArtwork },
+      { returnNewDocument: true }
+    );
+
+    // Check if the artwork was found and updated
+    if (!result) {
+      return res.status(404).json({ error: "artwork not found" });
+    }
+
+    res.status(204).json({
+      message: "artwork updated successfully",
+    });
+  } catch (error) {
+    console.error("Error updating artwork:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// DELETE route to delete a resource
+app.delete("/artworks/:id", async (req, res, next) => {
+  const artworkId = req.params.id;
+
+  //data validation
+  if (artworkId == null) {
+    return res.status(404).json({ error: "invalid input provided" });
+  }
+  // copied from ChatGPT some of the code below
+  try {
+    const db = await mongodb.connectDB();
+    const collection = db.collection("artworks");
+
+    // Delete the artwork by ID
+    const result = await collection.findOneAndDelete({
+      _id: new mongodb.ObjectId(artworkId),
+    });
+
+    // Check if the artwork was found and deleted
+    if (!result) {
+      return res.status(404).json({ error: "artwork not found" });
+    }
+
+    res.status(200).json({ message: "artwork deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting artwork:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
